351d71c50f3334d463ece0b85c1f3bfea0e28f30
Leyou Zhang <lzhang865@bloombert.net>
2019-08-15 23:02:29 -0400

    updated createImgSet

--- double-channel/scripts/createImgSet.m
+++ double-channel/scripts/createImgSet.m
@@ -1,27 +1,24 @@
 %% Data Path Setup
 
-folder = '/Users/lzhang865/Desktop/github/single-molecule-trace-selector-master/double-channel/data/traces';
+folder = '/Users/lzhang865/Desktop/github/single-molecule-trace-selector/double-channel/data/traces';
 pattern = '*.mltraces';
 files = dir(fullfile(folder,pattern));
 outputDir = '../data/images/';
 
 %% Create images and organize them into folders
-parfor i = 1 : length(files)
+parfor i = 1 : len(files)
     file = files(i);
-    if (contains(file.folder,'bad') && (rand() > 0.5))
-        continue;
+    dump = load(fullfile(file.folder,file.name),'-mat','traces');
+    trace = dump.traces;
+    for j = 1 : size(trace.donor,1)
+        [~,rawName,~] = fileparts(file.name);
+        if any(trace.label(j,:))
+            outputPath = strcat(outputDir,'accepted/');
+        else
+            outputPath = strcat(outputDir,'rejected/');
+        end
+        trace2img([trace.donor(j,:)', trace.acceptor(j,:)'],fullfile(outputPath,strcat(rawName,'-',num2str(j),'.jpg')));
     end
-    outputPath = strcat(outputDir,file.folder(length(rootDir)+1:end));
-    if ~exist(outputPath,'dir')
-        mkdir(outputPath);
-    end
-    traceData = readTimeTrace(fullfile(file.folder,file.name));
-    traceData = traceData{1};
-    n = size(traceData,1);
-    traceData = [reshape(traceData(1:n/2,:),1,[])' reshape(traceData(n/2+1:end,:),1,[])'];
-    [~,name,~] = fileparts(file.name);
-    createImage(traceData(:,:),fullfile(outputPath,strcat(name,'.png')));
-    figure(2);hold on;plot(traceData(:,1));plot(traceData(:,2));
 end
 
 
--- double-channel/scripts/trace2img.m
+++ double-channel/scripts/trace2img.m
@@ -5,9 +5,8 @@ function trace2img(traceData, file)
         'k','MarkerFaceColor','k','MarkerFaceAlpha',0,'MarkerEdgeAlpha',1.0);
     fig.Position = [200   200   256   256];
     graph = gca;
-    m = max(max(traceData(nstart:end,1)),max(traceData(nstart:end,2)));
-    graph.XLim = [-0.0,1.0] .* m;
-    graph.YLim = [-0.0,1.0] .* m;
+    graph.XLim = [-0.0,1.0] .* max(traceData(nstart:end,1));
+    graph.YLim = [-0.0,1.0] .* max(traceData(nstart:end,2));
     graph.XTick = [];
     graph.YTick = [];
     axis square;
