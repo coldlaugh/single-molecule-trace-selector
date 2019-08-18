530a04f2daa55a4cc4d6b52ab4adc5e8c23e7875
Leyou Zhang <leyou@umich.edu>
2019-08-17 23:26:30 -0400

    addressed comments from last commit

--- double-channel/scripts/train.m
+++ double-channel/scripts/train.m
@@ -1,95 +1,102 @@
-%% setup cnn network
+%% Setting up constants. 
 cnnNet = alexnet;
 numClasses = 2;
-inputSize = [227,227,3];
+inputSize = [64,64,3];
+
+if gpuDeviceCount()
+    computeEnv = 'gpu';
+else
+    computeEnv = 'parallel';
+end
+
+dataFolder = '../data/images/';
+dataSubFolders = {'accepted'};
+userMsg = waitbar(0,'Reading image data','Name','Reading image data');
+checkpointFolder = '../net/cnn/checkpoint/';
+checkpointFreq = 10;
+
+maxTrainEpochs = 200;
+batchSize = 500;
+algo = 'adam';
+learningRate = 0.01;
+dataUsageForTrain = 0.8;
+rejectedDropRate = 0.6;
+
+netFolder = '../net/cnn/';
+netOutput = 'cnn-alexnet.mat';
+
+%%  setup folders
+[~,~] = mkdir(checkpointFolder);
+[~,~] = mkdir(netFolder);
+
+%% Setup data store
+
+ds = fileDatastore(fullfile(dataFolder,dataSubFolders),'IncludeSubfolders',true, 'FileExtensions', '.jpg',...
+    'ReadFcn',@(loc)imresize(imread(loc),'Outputsize',inputSize(1:2)));
+
+%% Load train / test data from data store
+
+numTotal = length(ds.Files);
+numTrain = floor(dataUsageForTrain * numTotal);
+numTest = floor(numTotal - numTrain);
+
+indTrain = randperm(numTotal,numTrain);
+indTest = setdiff(1:numTotal,indTrain);
+indTest = indTest(randperm(length(indTest)));
+
+XTrain = zeros([inputSize,length(indTrain)]);
+XTest = zeros([inputSize,length(indTest)]);
+
+Y = categorical(contains(ds.Files,'accepted')); % label for each trace
+YTrain = Y(indTrain);
+YTest = Y(indTest);
+
+iTrain = 1;
+iTest = 1;
+for i = 1 : numTotal
+    if any(XTrain == i)
+        XTrain(:,:,:,iTrain) = read(ds);
+        iTrain = iTrain + 1;
+    else
+        XTest(:,:,:,iTest) = read(ds);
+        iTest = iTest + 1;
+    end
+    waitbar(i / numTotal,userMsg);
+end
+
+close(userMsg);
+%% Setup net
 
 endLayers = [
     fullyConnectedLayer(numClasses,'Name','fc','WeightLearnRateFactor',100,'BiasLearnRateFactor',100)
     softmaxLayer('Name','softmax')
-    classificationLayer('Name','classoutput')];
+    classificationLayer('Name','classoutput')
+    ];
+
 cnnLayers = [
-    cnnNet.Layers(1:end-3)
+    imageInputLayer(inputSize)
+    upsampleLayer()
+    cnnNet.Layers(2:end-3)
     endLayers
     ];
 
-%% load the training and testing data
-
-rootFolder = '../beta2.0/trainingDataImg/';
-categories = {'set1/goodMol','set1/badMol','set2/goodMol','set2/badMol','set3/goodMol','set3/badMol','set4/goodMol','set4/badMol','set5/goodMol'};
-ds = fileDatastore(fullfile(rootFolder,categories),'IncludeSubfolders',true, 'FileExtensions', '.png','ReadFcn',@(loc)imread(loc));
-
-f = waitbar(0,'Loading data','Name','Loading progress');
-
-files = ds.Files;
-
-X = cell(size(files));
-YR = cell(size(files)); % label for each frame
-Y = categorical(contains(files,'good')); % label for each trace
-
-for i = 1 : length(files)
-    T = read(ds);
-    X{i} = T;
-%     YR{i} = categorical(T{2});
-    waitbar(i/length(files),f);
-end
-
-close(f);
-
-%% randomly assign data to training set and testing set
-
-numTrain = floor(0.5 * length(X));
-numTest = length(X) - numTrain;
-
-indTrain = randperm(length(X),numTrain);
-indTest = setdiff(1:length(X),indTrain);
-indTest = indTest(randperm(length(indTest)));
-
-XTrain = zeros([inputSize,length(indTrain)]);
-for i = 1 : length(indTrain)
-    XTrain(:,:,:,i) = X{indTrain(i)};
-end
-YTrain = Y(indTrain);
-XTest = zeros([inputSize,length(indTest)]);
-for i = 1 : length(indTest)
-    XTest(:,:,:,i) = X{indTest(i)};
-end
-YTest = Y(indTest);
-XTest = XTest(:,:,:,~contains(files(indTest),'set5'));
-YTest = YTest(~contains(files(indTest),'set5'));
-
-%% set training options
-mkdir('checkPoint');
-maxEpochs = 200;
-numberOfWorkers = 2;
-miniBatchSize = 50 * numberOfWorkers;
-options = trainingOptions('adam',...
-    'InitialLearnRate',.001,...
-    'MaxEpochs',maxEpochs,...
-    'MiniBatchSize',miniBatchSize,...
+options = trainingOptions(...
+     algo,...
+    'InitialLearnRate',learningRate,...
+    'MaxEpochs',maxTrainEpochs,...
+    'MiniBatchSize',batchSize,...
     'Shuffle','every-epoch',...
-    'ExecutionEnvironment','parallel',...
+    'ExecutionEnvironment',computeEnv,...
     'Plots','training-progress',...
-    'CheckpointPath',fullfile(pwd(),'checkPoint/')...
+    'ValidationData',{XTest,YTest},...
+    'ValidationFrequency',floor(numTrain / batchSize * 5),...
+    'ValidationPatience',Inf,...
+    'Epsilon',10^-4,...
+    'CheckpointPath',''...
 );
 
 %% train CNN
-if strcmp(options.ExecutionEnvironment,'cpu')
-    [cnnNet,info] = trainNetwork(XTrain,YTrain,cnnLayers,options);
-elseif strcmp(options.ExecutionEnvironment,'gpu')
-    pool = gcp();
-%     addAttachedFiles(pool,...
-%         {
-%         '~/Desktop/FRET/beta2.0/imageConversionLayer.m',...
-%         '~/Desktop/FRET/beta2.0/TimeSeriesToImg.m'
-%         });
-%     updateAttachedFiles(pool)
-    spmd
-        [cnnNet,info] = trainNetwork(XTrain,YTrain,cnnLayers,options);
-    end
-    cnnNet = cnnNet{1};
-    info = info{1};
-elseif strcmp(options.ExecutionEnvironment,'parallel')
-    [cnnNet,info] = trainNetwork(XTrain,YTrain,cnnLayers,options);
-end
+
+[cnnNet,info] = trainNetwork(XTrain,YTrain,cnnLayers,options);
 cnnLayers = cnnNet.Layers;
-save('cnnNet.mat','cnnNet');
+save(fullfile(netFolder, netOutput),'cnnNet');
