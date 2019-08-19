18f94c388d0490c15f4b25135d2c722ce135944e
Leyou Zhang <lzhang865@bloombert.net>
2019-08-18 22:52:59 -0400

    updated images

--- double-channel/scripts/trace2img.m
+++ double-channel/scripts/trace2img.m
@@ -1,12 +1,12 @@
 function trace2img(traceData, file)
     fig = figure('Name','trace2img');
     nstart = 1;
-    filter = [1/6,2/3,1/6];
+    filter = [1/12,1/12,1/6,1/3,1/6,1/12,1/12];
     x = conv(traceData(nstart:end, 1), filter);
     y = conv(traceData(nstart:end, 2), filter);
     scatter(x,y,'MarkerEdgeColor',...
-        'k','MarkerFaceColor','k','MarkerFaceAlpha',0.2,'MarkerEdgeAlpha',0.2);
-    fig.Position = [100   100   200   200];
+        'k','MarkerFaceColor','k','MarkerFaceAlpha',0,'MarkerEdgeAlpha',0.5);
+    fig.Position = [100   100   300   300];
     graph = gca;
     graph.XLim = [-0.0,1.0] .* max(traceData(nstart:end,1));
     graph.YLim = [-0.0,1.0] .* max(traceData(nstart:end,2));
