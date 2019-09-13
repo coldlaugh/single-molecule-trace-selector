7ac13cb9a2b44d055ffd42df74bc24a3cfc79329
Leyou Zhang <lzhang865@bloombert.net>
2019-09-13 11:53:28 -0400

    added data from Jieming

--- double-channel/scripts/createSerialDataSet.m
+++ double-channel/scripts/createSerialDataSet.m
@@ -1,15 +1,14 @@
 %% Data Path Setup
-
 folder = '/Users/lzhang865/Desktop/github/single-molecule-trace-selector/double-channel/data/traces';
-pattern = '*.mltraces';
+pattern = 'W*.mltraces';
 files = dir(fullfile(folder,pattern));
 outputDir = '../data/serial/';
+dropoutRejected = 0.2;
 
 %% Create images and organize them into folders
 for i = 1 : length(files)
     file = files(i);
     dump = load(fullfile(file.folder,file.name),'-mat','traces');
-    trace = dump.traces;
     for j = 1 : size(trace.donor,1)
         [~,rawName,~] = fileparts(file.name);
         if (length(trace.donor(j,:)) <= 1000)
@@ -17,8 +16,10 @@ for i = 1 : length(files)
         end
         if any(trace.label(j,:))
             outputPath = strcat(outputDir,'accepted/');
-        else
+        elseif (rand() < dropoutRejected)
             outputPath = strcat(outputDir,'rejected/');
+        else
+            continue;
         end
         if ~exist(outputPath, 'dir')
             mkdir(outputPath);
