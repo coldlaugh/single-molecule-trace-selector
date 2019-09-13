04559f1ce9e80841a8926512809bf2f97ada57d3
Leyou Zhang <lzhang865@bloombert.net>
2019-09-13 12:24:09 -0400

    regenerated all serial data

--- double-channel/scripts/createImgDataSet.m
+++ double-channel/scripts/createImgDataSet.m
@@ -10,14 +10,15 @@ parfor i = 1 : length(files)
     file = files(i);
     dump = load(fullfile(file.folder,file.name),'-mat','traces');
     trace = dump.traces;
+    if ~trace.islabeled
+        disp(strcat("Skipping file ",file.name," because file is not labeled"));
+        continue
+    end
     for j = 1 : size(trace.donor,1)
         [~,rawName,~] = fileparts(file.name);
         if any(trace.label(j,:))
             outputPath = strcat(outputDir,'accepted/');
         else
-            if (rand() < 0.5)
-                continue;
-            end
             outputPath = strcat(outputDir,'rejected/');
         end
         trace2img([trace.donor(j,:)', trace.acceptor(j,:)'],fullfile(outputPath,strcat(rawName,'-',num2str(j),'.jpg')));
--- double-channel/scripts/createSerialDataSet.m
+++ double-channel/scripts/createSerialDataSet.m
@@ -1,18 +1,29 @@
 %% Data Path Setup
 folder = '/Users/lzhang865/Desktop/github/single-molecule-trace-selector/double-channel/data/traces';
-pattern = 'W*.mltraces';
+pattern = '*.mltraces';
 files = dir(fullfile(folder,pattern));
 outputDir = '../data/serial/';
-dropoutRejected = 0.2;
+dropoutRejected = 1;
 
 %% Create images and organize them into folders
 for i = 1 : length(files)
     file = files(i);
     dump = load(fullfile(file.folder,file.name),'-mat','traces');
+    trace = dump.traces;
+    if ~trace.islabeled
+        disp(strcat("Skipping file ",file.name," because file is not labeled"));
+        continue
+    end
     for j = 1 : size(trace.donor,1)
         [~,rawName,~] = fileparts(file.name);
         if (length(trace.donor(j,:)) <= 1000)
             continue;
+        end
+        if (length(trace.donor(j,:)) >= 5000)
+            trace.donor(j,1:5000) = trace.donor(j,1:5000);
+            trace.acceptor(j,1:5000) = trace.acceptor(j,1:5000);
+            trace.time = trace.time(1:5000);
+            trace.label(j,1:5000) = trace.label(j,1:5000);
         end
         if any(trace.label(j,:))
             outputPath = strcat(outputDir,'accepted/');
