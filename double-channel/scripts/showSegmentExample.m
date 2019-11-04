4ea5a574f18fc5e052773653a20dbb8c7fc0b488
Leyou Zhang <leyou@umich.edu>
2019-11-03 22:47:20 -0500

    new figures

--- double-channel/scripts/showSegmentExample.m
+++ double-channel/scripts/showSegmentExample.m
@@ -5,8 +5,8 @@
 
 
 read = @(loc) load(loc, '-mat');
-token = 'W7';
-close all;
+token = 'rib';
+% close all;
 for expt = 1 : 1
     for condition = 1 : 1
         exptFolder = strcat('../experiments/experiment',num2str(condition),'-',num2str(expt),'/');
@@ -26,10 +26,10 @@ for expt = 1 : 1
                 continue;
             end
             trace = read(fullfile(files.serialFolder,strcat(files.testSet{i},files.serialFormat)));
-%             if contains(files.testSet{i},'accepted')
-%                 disp("Showing ground truth")
-%                 plot_trace(trace.data, trace.data(3,:));
-%             end
+            if contains(files.testSet{i},'accepted')
+                disp("Showing ground truth")
+                plot_trace(trace.data, trace.data(3,:));
+            end
 %             if cnnData.testLabel(i)
 %                 disp("Showing cnn result")
 %                 segLabel = flatten(trace.data, segData.testLabel{i}, segData.numStack);
@@ -40,11 +40,11 @@ for expt = 1 : 1
 %                 segLabel = flatten(trace.data, segData.testLabel{i}, segData.numStack);
 %                 plot_trace(trace.data, segLabel)
 %             end
-            if rnnData.testLabel(i) && cnnData.testLabel(i)
-                disp("Showing cnn + rnn result")
-                segLabel = flatten(trace.data, segData.testLabel{i}, segData.numStack);
-                plot_trace(trace.data, segLabel)
-            end
+%             if rnnData.testLabel(i) && cnnData.testLabel(i)
+%                 disp("Showing cnn + rnn result")
+%                 segLabel = flatten(trace.data, segData.testLabel{i}, segData.numStack);
+%                 plot_trace(trace.data, segLabel)
+%             end
         end
     end
 end
@@ -61,15 +61,15 @@ function plot_trace(trace, segLabel)
     figure(1);cla;hold on;
     frame = 1 : length(trace);
     segLabel = boolean(segLabel);
-    p = plot(frame, trace(1,:),'b--');
+    p = plot(frame, trace(1,:),'--','Color',[0 0.5 0],'LineWidth',1);
     p.Color(4) = 0.1;
-    p = plot(frame, trace(2,:),'r--');
+    p = plot(frame, trace(2,:),'r--','LineWidth',1);
     p.Color(4) = 0.1;
-    p = plot(frame(segLabel), trace(1,segLabel),'b-');
+    p = plot(frame(segLabel), trace(1,segLabel),'--','Color',[0 0.5 0],'LineWidth',1);
     p.Color(4) = 0.8;
-    p = plot(frame(segLabel), trace(2,segLabel),'r-');
+    p = plot(frame(segLabel), trace(2,segLabel),'r--','LineWidth',1);
     p.Color(4) = 0.8;
-    xlim([0 2000])
+%     xlim([0 2000])
     pause
 end
 
