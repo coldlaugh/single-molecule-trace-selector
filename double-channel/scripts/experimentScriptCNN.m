f38f8394047eaedfc4a48e0d48c358ac76acf193
Leyou Zhang <leyou@umich.edu>
2020-01-31 00:56:16 -0500

    clean up double channel code

--- double-channel/scripts/experimentScriptCNN.m
+++
@@ -1,7 +0,0 @@
-for expt = 1 : 10
-    for condition = 1 : 2
-        exptFolder = strcat('../experiments/experiment',num2str(condition),'-',num2str(expt),'/');
-        train_CNN_func(exptFolder);
-    end
-end
-system('shutdown -s')
